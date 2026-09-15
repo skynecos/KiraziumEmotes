8ed2eabe7ac4904fe204bb21d34663958bdd0676
skynecos <skynecos@gmail.com>
2026-09-15 22:55:59 +0300

    build: add verified Nexo ItemsAdder and Oraxen APIs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,22 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://mvn.lumine.io/repository/maven-public/")
+    maven("https://repo.nexomc.com/releases")
+    maven("https://repo.oraxen.com/releases")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:${property("paperVersion")}")
     compileOnly("com.ticxo.modelengine:ModelEngine:${property("modelEngineVersion")}") {
+        isTransitive = false
+    }
+    compileOnly("com.nexomc:nexo:${property("nexoVersion")}") {
+        isTransitive = false
+    }
+    compileOnly("beer.devs:itemsadder-api:${property("itemsAdderApiVersion")}") {
+        isTransitive = false
+    }
+    compileOnly("io.th0rgal:oraxen:${property("oraxenVersion")}") {
         isTransitive = false
     }
 }
